e0dc41d7d9a0e53308d4114db563994e53aaeea1
DurandMathieu <mathieu.durand@lacapitale.com>
2019-09-02 18:58:47 -0400

    fix: better example for nestjs

--- examples/nestjs/swagger/generate.js
+++ examples/nestjs/swagger/generate.js
@@ -1,13 +1,19 @@
 const fs = require("fs");
 const path = require("path");
-const swaggerTemplate = require("../../../generator");
+const swaggerTemplate = require("../../../src/generator");
 const petstoreSwagger = require("./petstore.json");
 
 const templateTypesFile = fs.readFileSync(path.join(__dirname, "./template-types.hbs"), "utf8");
 const typesGeneratedCode = swaggerTemplate.generate(petstoreSwagger, templateTypesFile);
 
 const templateClientFile = fs.readFileSync(path.join(__dirname, "./template-client.hbs"), "utf8");
-const clientGeneratedCode = swaggerTemplate.generate(petstoreSwagger, templateClientFile, {clientName: "PetStoreClient"});
+const clientGeneratedCode = swaggerTemplate.generate(petstoreSwagger, templateClientFile, {
+    clientName: "PetStoreClient"
+});
 
-const imports = 'import { HttpService, Injectable } from "@nestjs/commun";\nimport * as uriTemplates from "uri-templates";';
-fs.writeFileSync("./swagger/petstore.client.generated.ts", imports + "\n\n" + typesGeneratedCode + "\n\n" + clientGeneratedCode);
+const imports =
+    'import { HttpService } from "@nestjs/common";\nimport { map } from "rxjs/operators";\nimport { AxiosRequestConfig } from "axios";\nimport * as uriTemplates from "uri-templates";';
+fs.writeFileSync(
+    "./swagger/petstore.client.generated.ts",
+    imports + "\n\n" + typesGeneratedCode + "\n\n" + clientGeneratedCode
+);
